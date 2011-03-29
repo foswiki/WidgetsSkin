87c7bcb4cd66da2f4740002ee9a1d5f11ada9142
WillNorris <WillNorris@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-03-29 20:59:45 +0000

    Item10570: make work better with configure and FastInstaller
    git-svn-id: http://svn.foswiki.org/trunk/WidgetsSkin@11271 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/WidgetsSkin.pm
+++ lib/Foswiki/Contrib/WidgetsSkin.pm
@@ -1,5 +1,8 @@
 package Foswiki::Contrib::WidgetsSkin;
-use vars qw( $VERSION );
-$VERSION = '$Rev$';
+
+our $VERSION = '$Rev$';
+our $RELEASE = '2.0.1';
+our $SHORTDESCRIPTION =
+'WidgetsSkin provides a themeing framework based on Movable Type 4 (MT4).  This skin is very easy to style and is extremely flexible.  Many additional skin components are provided as resuable "widgets".';
 
 1;
