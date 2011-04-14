e48abac61627cded46f0b9a77438a071fc5e14d6
WillNorris <WillNorris@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-04-14 14:46:26 +0000

    Item10570: restyle login button (real foswiki button style)
    git-svn-id: http://svn.foswiki.org/trunk/WidgetsSkin@11457 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/WidgetsSkin.pm
+++ lib/Foswiki/Contrib/WidgetsSkin.pm
@@ -1,8 +1,8 @@
 package Foswiki::Contrib::WidgetsSkin;
 
 our $VERSION = '$Rev$';
-our $RELEASE = '2.0.1';
+our $RELEASE = '2.0.2';
 our $SHORTDESCRIPTION =
-'WidgetsSkin provides a themeing framework based on Movable Type 4 (MT4).  This skin is very easy to style and is extremely flexible.  Many additional skin components are provided as resuable "widgets".';
+'WidgetsSkin provides a themeing framework based on Movable Type 4 (MT4).  This skin is very easy to style and is extremely flexible.  Many additional skin UI components are provided as resuable "widgets".';
 
 1;
